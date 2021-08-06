c6c325bc6adccc8b27a30ff3abb2a808a1be5e4a
Rianna Castro <riannaxcastro@gmail.com>
2021-08-05 21:33:50 -1000

    added drafts of forms

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -13,7 +13,20 @@ class CarsController < ApplicationController
     end
 
     def create
+        @car = Car.new(car_params)
+        
+        if @car.save
+            redirect_to cars_path
+        else
+            render :new
+        end
 
+    end
+
+    private
+
+    def car_params
+        params.require(:car).permit(:year, :model, :color)
     end
 
 
