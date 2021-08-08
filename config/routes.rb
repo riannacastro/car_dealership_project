079c845d1e83d8cf502b25898e149ad6ba1fa52a
Rianna Castro <riannaxcastro@gmail.com>
2021-08-07 15:46:02 -1000

    made changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# get '/cars', to: "cars#index"
-# get '/cars/:id', to: "cars#show", as: 'car'
+  get '/signup', to: "users#new"
+  post '/signup', to: "users#create"
+  get '/login', to: "sessions#new"
+  get '/login', to: "sessions#create"
 resources :cars
 resources :brands do 
   resources :cars, only: [:new, :create, :index]
 end
 resources :users
-get '/signup', to: "users#new"
-post '/signup', to: "users#create"
+resources :countries
 end
