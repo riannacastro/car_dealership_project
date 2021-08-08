5db1898b70885f646abe989c062e85d2e50906ee
Rianna Castro <riannaxcastro@gmail.com>
2021-08-07 15:51:07 -1000

    added sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+    
+    def welcome
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def omniauth
+    end
+
+    def destroy
+        
+    end
+
+end
