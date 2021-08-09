1e923a77dc322d5f0c5d1c48d87a126cc631086f
Rianna Castro <riannaxcastro@gmail.com>
2021-08-08 20:29:47 -1000

    created nested form

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,11 @@
 class Car < ApplicationRecord
     belongs_to :user
     belongs_to :brand
+    accepts_nested_attributes_for :brand
+
+    validates :year, presence: true, length: { is: 4 }
+    validates :model, presence: true
+    validates :color, presence: true
 
     def car_details
     "#{self.year} #{self.model} - #{self.color}"
