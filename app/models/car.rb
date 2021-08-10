d1ab50d10cb60eb41fd64f669e35c982d23b9115
Rianna Castro <riannaxcastro@gmail.com>
2021-08-10 07:39:37 -1000

    edit validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -7,12 +7,12 @@ class Car < ApplicationRecord
         if !hash_of_attributes["name"].blank? && !hash_of_attributes["year_created"].blank?
             self.brand = Brand.find_or_create_by(hash_of_attributes)
         end
-        
+
     end
 
-    validates :year, presence: true, length: { is: 4 }
-    validates :model, presence: true
-    validates :color, presence: true
+    validates :year, numericality: {greater_than: 1900, less_than: 2021, message: "Please choose the correct year."}
+    validates :color, presence: true, uniqueness: {scope: [:model]}
+    
 
     def car_details
     "#{self.year} #{self.model} - #{self.color}"
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get '/auth/google_oauth2/callback', to: "sessions#omniauth"
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
-  delete '/logout', to: "sessions#destroy"
+  
   get '/signup', to: "users#new"
   post '/signup', to: "users#create"
-  
+  delete '/logout', to: "sessions#destroy"
   
 resources :cars
 resources :brands do 
