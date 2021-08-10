2ec5d9c82109af6863f016a26485a5d5602129d0
Rianna Castro <riannaxcastro@gmail.com>
2021-08-10 07:53:20 -1000

    added brands page

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,4 +3,9 @@ class Brand < ApplicationRecord
     belongs_to :country
     validates :name, presence: true
     validates :year_created, presence: true
+
+    def brand_details
+        "#{self.name} - #{self.year_created}"
+    end
+
 end
