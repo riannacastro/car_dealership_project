7608a6fd3bc61b6356a7e1a782d9269424136bcf
Rianna Castro <riannaxcastro@gmail.com>
2021-08-12 09:14:00 -1000

    edited home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     private
 
     def redirect_if_not_logged_in?
-        redirect_to login_path if !logged_in?
+        redirect_to root_path if !logged_in?
     end
 
 end
